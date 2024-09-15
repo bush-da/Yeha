bf40ba7aa05a025e35dc7e971c4b3456419b4ba1
Daniel Tujuma <danielsevani12@gmail.com>
2024-09-15 23:27:57 +0300

    represent follower or following to user

---
+++ models/follower.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines the Follower class for user following system"""
+from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
+
+class Follower(BaseModel, Base):
+    """Represents a user following another user"""
+    __tablename__ = 'followers'
+
+    follower_id = Column(String(60), ForeignKey('users.id'), primary_key=True)
+    followed_id = Column(String(60), ForeignKey('users.id'), primary_key=True)
+
+    #Relationship
+    follower = relationship('User', foreign_keys=[follower_id], backref='following')
+    followed = relationship('User', foreign_keys=[followed_id], backref='followers')
