31cfc16384ee1ebec46a51dda17cfeda08d3ccd5
Daniel Tujuma <danielsevani12@gmail.com>
2024-09-15 23:24:58 +0300

    post comment

---
+++ models/comment.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines the Comment class"""
+from sqlalchemy import Column, Text, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+
+class Comment(BaseModel, Base):
+    """Represents a comment on a post"""
+    __tablename__ = 'comments'
+
+    content = Column(Text, nullable=False)
+    author_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    post_id = Column(String(60), ForeignKey('posts.id'), nullable=False)
+
+    # Relationships
+    author = relationship("User", back_populates="comments")
+    post = relationship("Post", back_populates="comments")
