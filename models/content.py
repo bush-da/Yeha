bb730fa6174fe1fd0a60026b2469772276921b31
Daniel Tujuma <danielsevani12@gmail.com>
2024-09-18 11:50:24 +0300

    add text column

--- models/content.py
+++ models/content.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines the Content class"""
-from sqlalchemy import Column, Text, ForeignKey
+from sqlalchemy import Column, Text, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
@@ -8,7 +8,9 @@ class Content(BaseModel, Base):
     """Content attributes that will be posted on the website"""
     __tablename__ = 'contents'
 
-    file_url = Column(Text, nullable=False)
+    file_url = Column(Text, nullable=True)
+    paragraph = Column(Integer, nullable=False)
+    content = Column(Text, nullable=True)
     post_id = Column(String(60), ForeignKey('posts.id'), nullable=False)
 
     # Relationship
