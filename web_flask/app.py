4028911e3e87749f49cc6c0d0a3fdb8e74da9e3e
Daniel Tujuma <danielsevani12@gmail.com>
2024-09-18 12:09:53 +0300

    define all routes and function

---
+++ web_flask/app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from models import storage
+from web_flask.routes.auth_routes import auth_bp
+from web_flask.routes.post_routes import post_bp
+from web_flask.routes.comment_routes import comment_bp
+from web_flask.routes.like_routes import like_bp
+from web_flask.routes.about import about_bp
+from web_flask.routes.home import home_bp
+from web_flask.routes.profile_routes import profile_bp  # Import the profile blueprint
+
+def create_app():
+    """Create and configure the Flask application."""
+    app = Flask(__name__)
+    # Configuration
+    app.config.from_object('web_flask.config.Config')
+
+    # Register Blueprints
+    app.register_blueprint(home_bp)
+    app.register_blueprint(about_bp)
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(post_bp)
+    app.register_blueprint(comment_bp)
+    app.register_blueprint(like_bp)
+    app.register_blueprint(profile_bp)  # Register the profile blueprint
+
+    @app.teardown_appcontext
+    def teardown(exception):
+        """Close the database session."""
+        storage.close()
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(host='0.0.0.0', port=5000, debug=True)
