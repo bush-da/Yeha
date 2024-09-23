2a4ce1524ca59f1010f2f4106b8335322076070d
Daniel Tujuma <danielsevani12@gmail.com>
2024-09-23 23:32:39 +0300

    like button logic

--- web_flask/routes/like_routes.py
+++ web_flask/routes/like_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, session, redirect, url_for, flash
+from flask import Blueprint, session, redirect, url_for, flash, request
 from models import storage
 from models.like import Like
 from models.post import Post
@@ -13,13 +13,15 @@ def like_post(post_id):
         flash('Please log in to like posts.')
         return redirect(url_for('auth.login'))
 
-    all_likes = storage.all(Like)
-    like_key = f"Like.{user_id}_{post_id}"
-    if like_key in all_likes:
-        flash('You have already liked this post.')
-        return redirect(request.referrer)
+    all_likes = storage.all(Like).values()
+    for likes in all_likes:
+        if likes.author_id == user_id and likes.post_id == post_id:
+            storage.delete(likes)
+            storage.save()
+            return redirect(request.referrer)
 
-    new_like = Like(user_id=user_id, post_id=post_id)
+
+    new_like = Like(author_id=user_id, post_id=post_id)
     storage.new(new_like)
     storage.save()
 
