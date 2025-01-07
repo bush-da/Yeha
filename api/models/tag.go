cb1985167f2415a002c5dc536b30cda74c89b5b8
Daniel Tujuma <danielsevani12@gmail.com>
2025-01-08 00:27:04 +0300

    post tags model

---
+++ api/models/tag.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+// Tag represents a tag for posts (e.g., 'Python', 'Web Development').
+type Tag struct {
+	ID    uuid.UUID `gorm:"type:char(36);primaryKey" json:"id"`
+	Name  string    `gorm:"type:varchar(45);unique;not null;index" json:"name"`
+	Posts []Post    `gorm:"many2many:post_tags;" json:"posts"`
+}
+
+// BeforeCreate hook to generate UUID before saving to DB
+func (tag *Tag) BeforeCreate(tx *gorm.DB) (err error) {
+	if tag.ID == uuid.Nil {
+		tag.ID = uuid.New() // Generate a new UUID if not set
+	}
+	return
+}
