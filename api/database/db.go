84afc01144c634d8195046ae030d9050f0f9a859
Daniel Tujuma <danielsevani12@gmail.com>
2025-01-08 00:36:16 +0300

    create dsn and connect to database

---
+++ api/database/db.go
@@ -0,0 +1,35 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+// ConnectDatabase initializes and connects to the database
+func ConnectDatabase() (*gorm.DB, error) {
+	// Fetch database credentials from environment variables
+	user := os.Getenv("MYSQL_USER")
+	password := os.Getenv("MYSQL_PASSWORD")
+	host := os.Getenv("MYSQL_HOST")
+	dbName := os.Getenv("MYSQL_DB")
+
+	// Create DSN (Data Source Name)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, dbName)
+
+	// Open the database connection
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err // Return error if connection fails
+	}
+
+	log.Println("MySQL database connected successfully!")
+	DB = db // Store DB connection globally
+
+	return db, nil // Return the DB instance
+}
