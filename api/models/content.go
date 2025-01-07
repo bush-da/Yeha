4af4e097e2e6983f7de6bc1d757d31294a8fc786
Daniel Tujuma <danielsevani12@gmail.com>
2025-01-08 00:28:35 +0300

    content model for posts

---
+++ api/models/content.go
@@ -0,0 +1,32 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+// Content represents the content that will be posted on the website.
+type Content struct {
+	ID          uuid.UUID `gorm:"type:char(36);primaryKey" json:"id"`
+	FileURL     string    `gorm:"type:text" json:"file_url"`
+	Paragraph   int       `gorm:"not null" json:"paragraph"`
+	Content     string    `gorm:"type:text" json:"content"`
+	PostID      uuid.UUID `gorm:"type:char(36);not null;index" json:"post_id"`
+	ContentType string    `gorm:"type:enum('text', 'image', 'video');default:'text'" json:"content_type"`
+
+	// Relationship with Post
+	Post Post `gorm:"foreignKey:PostID;constraint:OnDelete:CASCADE" json:"post"`
+}
+
+// BeforeCreate hook to generate UUID before saving to DB
+func (content *Content) BeforeCreate(tx *gorm.DB) (err error) {
+	if content.ID == uuid.Nil {
+		content.ID = uuid.New() // Generate a new UUID if not set
+	}
+	return
+}
+
+// TableName overrides the table name.
+func (Content) TableName() string {
+	return "contents"
+}
