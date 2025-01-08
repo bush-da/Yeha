957959903b1135e36857e64fc19b3894edbb427b
Daniel Tujuma <danielsevani12@gmail.com>
2025-01-08 13:05:03 +0300

    route for users

---
+++ api/routes/user_routes.go
@@ -0,0 +1,26 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"yeha-api/controllers"
+	"yeha-api/middleware"
+)
+
+// UserRoutes defines routes related to user management
+func UserRoutes(rg *gin.RouterGroup, db *gorm.DB) {
+	userController := controllers.NewUserController(db)
+
+	// Public Routes
+	rg.POST("/register", userController.RegisterUser)
+	rg.POST("/login", userController.LoginUser)
+
+	// Protected Routes
+	protected := rg.Group("/")
+	protected.Use(middleware.AuthMiddleware()) // Add auth middleware here
+
+	// Require authentication for the following routes
+	protected.GET("/:id", userController.GetUserProfile)
+	protected.PUT("/:id", userController.UpdateUserProfile)
+	protected.DELETE("/:id", userController.DeleteUser)
+}
