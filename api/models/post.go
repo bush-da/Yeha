f78b4f0a09449da5a43056bb948fc4417d948295
Daniel Tujuma <danielsevani12@gmail.com>
2025-01-08 15:27:54 +0300

    fix the post BeforeDelete hook

--- api/models/post.go
+++ api/models/post.go
@@ -12,11 +12,17 @@ type Post struct {
 	AuthorID uuid.UUID `gorm:"type:char(36);not null" json:"author_id"`
 
 	// Relationships
-	Reports  []Report  `gorm:"foreignKey:PostID" json:"reports"`
-	Contents []Content `gorm:"foreignKey:PostID" json:"contents"`
-	Comments []Comment `gorm:"foreignKey:PostID" json:"comments"`
-	Likes    []Like    `gorm:"foreignKey:PostID" json:"likes"`
-	Tags     []Tag     `gorm:"many2many:post_tags;" json:"tags"`
+	// Reports  []Report  `gorm:"foreignKey:PostID" json:"reports"`
+	// Contents []Content `gorm:"foreignKey:PostID" json:"contents"`
+	// Comments []Comment `gorm:"foreignKey:PostID" json:"comments"`
+	// Likes    []Like    `gorm:"foreignKey:PostID" json:"likes"`
+	// Tags     []Tag     `gorm:"many2many:post_tags;" json:"tags"`
+	// Post Model Relationships
+	Reports  []Report  `gorm:"foreignKey:PostID;constraint:OnDelete:CASCADE" json:"reports"`
+	Contents []Content `gorm:"foreignKey:PostID;constraint:OnDelete:CASCADE" json:"contents"`
+	Comments []Comment `gorm:"foreignKey:PostID;constraint:OnDelete:CASCADE" json:"comments"`
+	Likes    []Like    `gorm:"foreignKey:PostID;constraint:OnDelete:CASCADE" json:"likes"`
+	Tags     []Tag     `gorm:"many2many:post_tags;" json:"tags"` // No need for CASCADE here; managed in PostTag
 
 	Author User `gorm:"foreignKey:AuthorID" json:"author"`
 }
@@ -44,38 +50,38 @@ func (p *Post) AddTag(tag Tag, db *gorm.DB) error {
 	return err
 }
 
-// RemoveTag removes a tag from the post.
-func (p *Post) RemoveTag(tag Tag, db *gorm.DB) error {
-	err := db.Model(&p).Association("Tags").Delete(&tag)
-	return err
-}
+// // RemoveTag removes a tag from the post.
+// func (p *Post) RemoveTag(tag Tag, db *gorm.DB) error {
+// 	err := db.Model(&p).Association("Tags").Delete(&tag)
+// 	return err
+// }
 
-// BeforeDelete cleans up unused tags before deleting the post.
-func (p *Post) BeforeDelete(tx *gorm.DB) error {
-	var tags []Tag
-	tx.Model(&p).Association("Tags").Find(&tags)
+// //BeforeDelete cleans up unused tags before deleting the post.
+// func (p *Post) BeforeDelete(tx *gorm.DB) error {
+// 	var tags []Tag
+// 	tx.Model(&p).Association("Tags").Find(&tags)
 
-	// Delete the post
-	if err := tx.Delete(&p).Error; err != nil {
-		return err
-	}
+// 	// Delete the post
+// 	if err := tx.Delete(&p).Error; err != nil {
+// 		return err
+// 	}
 
-	// Clean up tags with no posts
-	for _, tag := range tags {
-		var count int64
-		// Correct the call to Count() without passing an argument
-		tx.Model(&tag).Association("Posts").Count()
+// 	// Clean up tags with no posts
+// 	for _, tag := range tags {
+// 		var count int64
+// 		// Correct the call to Count() without passing an argument
+// 		tx.Model(&tag).Association("Posts").Count()
 
-		// If the count is zero, delete the tag
-		if count == 0 {
-			if err := tx.Delete(&tag).Error; err != nil {
-				return err
-			}
-		}
-	}
+// 		// If the count is zero, delete the tag
+// 		if count == 0 {
+// 			if err := tx.Delete(&tag).Error; err != nil {
+// 				return err
+// 			}
+// 		}
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
 // BeforeCreate hook ensures UUID is set for ID field
 func (post *Post) BeforeCreate(tx *gorm.DB) (err error) {
