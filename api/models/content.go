3025ce9dd03924b9ce9c6474023236315fcb6570
Daniel Tujuma <danielsevani12@gmail.com>
2025-01-16 21:43:53 +0300

    fix separation b/n contents

--- api/models/content.go
+++ api/models/content.go
@@ -1,18 +1,22 @@
 package models
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 // Content represents the content that will be posted on the website.
 type Content struct {
-	ID          uuid.UUID `gorm:"type:char(36);primaryKey" json:"id"`
-	FileURL     string    `gorm:"type:text" json:"file_url"`
-	Paragraph   int       `gorm:"not null" json:"paragraph"`
-	Content     string    `gorm:"type:text" json:"content"`
-	PostID      uuid.UUID `gorm:"type:char(36);not null;index" json:"post_id"`
-	ContentType string    `gorm:"type:enum('text', 'image', 'video');default:'text'" json:"content_type"`
+	ID        uuid.UUID `gorm:"type:char(36);primaryKey" json:"id"`
+	FileURL   string    `gorm:"type:text" json:"file_url"`
+	Paragraph int       `gorm:"not null" json:"paragraph"`
+	Content   string    `gorm:"type:text" json:"content"`
+	PostID    uuid.UUID `gorm:"type:char(36);not null;index" json:"post_id"`
+
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
 
 	// Relationship with Post
 	Post Post `gorm:"foreignKey:PostID;constraint:OnDelete:CASCADE" json:"post"`
