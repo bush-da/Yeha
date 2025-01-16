679519672507ec4c5b04d088b1f699aef920aee1
Daniel Tujuma <danielsevani12@gmail.com>
2025-01-16 21:45:31 +0300

    fix relation with post and tag

--- api/models/post_tag.go
+++ api/models/post_tag.go
@@ -8,13 +8,9 @@ import (
 type PostTag struct {
 	PostID uuid.UUID `gorm:"type:char(36);primaryKey" json:"post_id"`
 	TagID  uuid.UUID `gorm:"type:char(36);primaryKey" json:"tag_id"`
-
-	// Relationships
-	Post Post `gorm:"foreignKey:PostID;constraint:OnDelete:CASCADE" json:"post"`
-	Tag  Tag  `gorm:"foreignKey:TagID;constraint:OnDelete:CASCADE" json:"tag"`
 }
 
-// TableName overrides the table name for the join table
+// TableName overrides the default table name for the join table.
 func (PostTag) TableName() string {
 	return "post_tags"
 }
