b3c3cb25d0acbde95635a80bbb971e4d6bdc30ae
Daniel Tujuma <danielsevani12@gmail.com>
2025-01-16 21:47:07 +0300

    add routes only used by authenticated user

--- api/routes/comment_routes.go
+++ api/routes/comment_routes.go
@@ -11,13 +11,13 @@ func CommentRoutes(rg *gin.RouterGroup, db *gorm.DB) {
 	commentController := controllers.CommentController{DB: db}
 
 	// Public Route: Get all comments for a post (no authentication required)
-	rg.GET("/posts/:post_id/comments", commentController.GetComments)
+	rg.GET("/:post_id/comments", commentController.GetComments)
 
 	// Protected Routes (require authentication)
-	protected := rg.Group("/")
+	protected := rg.Group("/:post_id/comment")
 	protected.Use(middleware.AuthMiddleware()) // Apply AuthMiddleware only to these routes
 	{
-		protected.POST("/:post_id", commentController.CreateComment)      // Create Comment
+		protected.POST("/", commentController.CreateComment)              // Create Comment
 		protected.DELETE("/:comment_id", commentController.DeleteComment) // Delete Comment
 	}
 }
