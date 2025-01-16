9591db2cf068d97f3d001f52508b46649d132869
Daniel Tujuma <danielsevani12@gmail.com>
2025-01-16 21:44:41 +0300

    fix with post relation

--- api/models/like.go
+++ api/models/like.go
@@ -3,13 +3,16 @@ package models
 import (
 	"github.com/google/uuid"
 	"gorm.io/gorm"
+	"time"
 )
 
 // Like represents a 'like' on a post.
 type Like struct {
-	ID       uuid.UUID `gorm:"type:char(36);primaryKey" json:"id"`
-	AuthorID uuid.UUID `gorm:"type:char(36);not null" json:"author_id"`
-	PostID   uuid.UUID `gorm:"type:char(36);not null" json:"post_id"`
+	ID        uuid.UUID `gorm:"type:char(36);primaryKey" json:"id"`
+	CreatedAt time.Time `gorm:"type:datetime;default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt time.Time `gorm:"type:datetime;default:CURRENT_TIMESTAMP;autoUpdateTime" json:"updated_at"`
+	AuthorID  uuid.UUID `gorm:"type:char(36);not null" json:"author_id"`
+	PostID    uuid.UUID `gorm:"type:char(36);not null" json:"post_id"`
 
 	// Relationships
 	Author User `gorm:"foreignKey:AuthorID;constraint:OnDelete:CASCADE" json:"author"`
@@ -24,10 +27,7 @@ func (Like) TableName() string {
 // BeforeCreate hook to ensure UUID generation for composite keys
 func (like *Like) BeforeCreate(tx *gorm.DB) (err error) {
 	if like.ID == uuid.Nil {
-		like.ID = uuid.New() // Generate a new UUID for AuthorID if not set
-	}
-	if like.PostID == uuid.Nil {
-		like.PostID = uuid.New() // Generate a new UUID for PostID if not set
+		like.ID = uuid.New() // Generate a new UUID for ID if not set
 	}
 	return
 }
